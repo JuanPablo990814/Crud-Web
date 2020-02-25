32fe788aabfa7bb12736b22d8f2e6f98f56907b6
JuanPablo990814 <49729106+JuanPablo990814@users.noreply.github.com>
2020-02-25 01:17:09 -0500

    Avance en los formularios de pedidos

---
+++ PruebaNet.Datos/Variables_Locales.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaNet.Datos
+{
+    public class Variables_Locales
+    {
+        public Boolean formulario { get; set; }
+        
+    }
+}
--- PruebaNet.WebApi/Pages/Pedidos/InicioPedido.cshtml.cs
+++ PruebaNet.WebApi/Pages/Pedidos/InicioPedido.cshtml.cs
@@ -11,6 +11,7 @@ namespace PruebaNet.WebApi
 {
     public class InicioPedidoModel : PageModel
     {
+
         private readonly ApplicationDbContext _db;
 
         public InicioPedidoModel(ApplicationDbContext db)
@@ -18,20 +19,30 @@ namespace PruebaNet.WebApi
             _db = db;
         }
 
+        
+
         public IEnumerable<Clientes> Clientes { get; set; }
         public Pedidos Pedidos { get; set; }
+        public Producto Producto { get; set; }
 
+
+        //Variables_Locales Variables;    
 
         [TempData]
         public string Mensaje { get; set; }
 
-        public async Task OnGet()
+        [TempData]
+        public Boolean formulario { get; set; }
+
+        public async Task OnGet(int id)
         {
             Clientes = await _db.Clientes.ToListAsync();
+            Producto = await _db.Producto.FindAsync(id);
         }
 
 
-        public IActionResult OnPostBuscador(int cedula, Boolean form)
+
+        public IActionResult OnPostBuscador(int cedula)
         {
             ///////Consulta por la cedula
             var cliente = _db.Clientes.FirstOrDefault(x => x.cedula == cedula);
@@ -39,16 +50,42 @@ namespace PruebaNet.WebApi
 
             if (cliente != null)
             {
+                
                 cliente.id = cedula;
                 //y devulve la id para iniciar el OnGet de Clientes.cshtml.cs que realiza la consulta de los datos con la id
-                form = true;
+
+                formulario = true;
                 
-                return RedirectToPage("InicioPedido", new { form });
+            
+                return RedirectToPage("", new { formulario });
             }
 
             Mensaje = "No se ha encontrado registro de la cedula en la base de datos, registra al usuario primero";
 
             return RedirectToPage("");
         }
+
+        public IActionResult OnPostAggProducto(int pluproducto)
+        {
+            ///////Consulta por la cedula
+            var producto = _db.Producto.FirstOrDefault(x => x.plu == pluproducto);
+            int id;
+
+            if (producto != null)
+            {
+                producto.plu = pluproducto;
+                //y devulve la id para iniciar el OnGet de Clientes.cshtml.cs que realiza la consulta de los datos con la id
+
+                id = pluproducto;
+
+                return RedirectToPage("InicioPedido", new { id });
+            }
+
+            Mensaje = "No se ha encontrado registro de la cedula en la base de datos";
+
+            return RedirectToPage("");
+        }
+
+
     }
 }
